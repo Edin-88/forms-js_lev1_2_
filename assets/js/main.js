5d4db8215b6144a1c74661bef8579d7e721833f9
Your Name <selmanovic.edin1@gmail.com>
2023-01-11 14:06:01 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Button in JS holen
+
+let numberInput = document.getElementById('number')
+
+let value = document.getElementById('value')
+
+
+//Funktion zum berechnen des Alters
+
+function berechneMeinAlter() {
+  const userInput = number.value;
+  const age = 2023 - userInput;
+
+  value.innerHTML = age;
+}
+
